266b8d917cf87ff19a35ba17d44f45c0fddd3984
Nick Turner <dimesteaksNT@gmail.com>
2016-11-11 11:43:08 -0600

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * Created by NicholasTurner on 11/11/16.
+ */
+public class HelloWorld {
+    //psvm
+    public static void main(String[] args) {
+        //sout
+        System.out.println("Hello world!!");
+        System.out.print("Hello Mammoth!!");
+        System.out.printf(
+                "\nHello %s, today is %s, and it's the %dth day of the week ",
+                "Codeup",
+                "Friday",
+                5
+        );
+    }
+}
